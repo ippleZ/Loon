43c18aebaf9d389ce7be673cb67ab2958e10e963
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-07-28 11:33:36 +0800

    Update caiyun.js

--- script/caiyun.js
+++ script/caiyun.js
@@ -7,24 +7,44 @@ if (typeof $response === "undefined") {
   $done({ headers: header });
 } else {
   let obj = JSON.parse($response.body);
-  if (url.includes("/v1/activity")) {
-    // 彩云推广
-    if (["&type_id=A03&"]?.includes(url)) {
-      // 彩云AI
+  if (url.includes("/api.caiyunapp.com/v1/activity")) {
+    if (url.includes("&type_id=A03&")) {
+      // 底栏控制项目 主页图标 天气助手 彩云ai
       if (obj?.interval) {
         obj.interval = 2592000; // 30天===2592000秒
       }
       if (obj?.activities?.length > 0) {
-        let newActs = [];
         for (let item of obj.activities) {
-          if (item?.type === "tabbar" && item?.feature) {
+          if (item?.name && item?.type && item?.feature) {
             item.feature = false;
-          } else {
-            continue;
           }
-          newActs.push(item);
         }
-        obj.activities = newActs;
+      }
+    } else {
+      // 其他请求
+      obj = {
+        status: "ok",
+        interval: 2592000,
+        id: "1",
+        activities: [
+          {
+            items: [{ text: "", image_light: "", link: "", activity_name: "", id: "1", image_dark: "" }],
+            type: "activity_icon",
+            name: "",
+            carousel: "5000"
+          }
+        ]
+      };
+    }
+  } else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
+    // 彩云推广
+    if (["&type_id=A03&"]?.includes(url)) {
+      // 天气助手 彩云ai
+      if (obj?.interval) {
+        obj.interval = 2592000; // 30天===2592000秒
+      }
+      if (obj?.activities?.length > 0) {
+        obj.activities = [];
       }
     } else {
       // 其他请求
@@ -45,10 +65,10 @@ if (typeof $response === "undefined") {
   } else if (url.includes("/v1/vip_info")) {
     // 我的页面
     if (obj.vip) {
-      obj.vip.expires_time = "4102329600";
+      obj.vip.expires_time = "4030000000";
     }
     if (obj.svip) {
-      obj.svip.expires_time = "4102329600";
+      obj.svip.expires_time = "4030000000";
     }
   } else if (url.includes("/v2/user")) {
     // 我的页面
@@ -56,20 +76,20 @@ if (typeof $response === "undefined") {
       obj.result.svip_given = 730;
       obj.result.is_phone_verified = true;
       obj.result.is_xy_vip = true;
-      obj.result.vip_expired_at = 4102329600;
+      obj.result.vip_expired_at = 4102329600.16;
       obj.result.is_vip = true;
-      obj.result.xy_svip_expire = 4102329600;
+      obj.result.xy_svip_expire = 4102329600.16;
       if (obj.result.wt) {
         if (obj.result.wt.vip) {
           obj.result.wt.vip.enabled = true;
-          obj.result.wt.vip.expired_at = 4102329600;
-          obj.result.wt.vip.svip_expired_at = 4102329600;
+          obj.result.wt.vip.expired_at = 4102329600.16;
+          obj.result.wt.vip.svip_expired_at = 4102329600.16;
         }
         obj.result.wt.svip_given = 730;
       }
       obj.result.is_primary = true;
-      obj.result.xy_vip_expire = 4102329600;
-      obj.result.svip_expired_at = 4102329600;
+      obj.result.xy_vip_expire = 4102329600.16;
+      obj.result.svip_expired_at = 4102329600.16;
       obj.result.vip_type = "s";
     }
   }
