81e9c6f562ca14de076c01151117d4ad05ae7d61
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-09-27 11:16:29 +0800

    Update cmb.js

--- script/cmb.js
+++ script/cmb.js
@@ -7,11 +7,14 @@ function processValue(value) {
     return num > 0 ? (num * 2).toFixed(2) : (num / 2).toFixed(2);
 }
 
+// 初始化一个全局变量来存储总和
+let totalSum = 0;
+
 if (url.includes('/iincomereport/income/calendar')) {
-    // 针对 calendar 的逻辑
     if (obj.bizResult?.data?.ernInfo) {
         obj.bizResult.data.ernInfo.forEach(entry => {
             if (entry.ernMon) {
+                totalSum += parseFloat(entry.ernMon); // 累加总和
                 entry.ernMon = processValue(entry.ernMon);
             }
             if (entry.ernRat) {
@@ -20,16 +23,19 @@ if (url.includes('/iincomereport/income/calendar')) {
         });
     }
 } else if (url.includes('/iincomereport/incomecommon/incomeflow')) {
-    // 针对 incomeflow 的逻辑
     const excludeNames = ["朝朝宝", "周周宝", "月月宝", "季季宝", "快赎专区"];
     
     if (obj.bizResult?.data?.prdDtl) {
         obj.bizResult.data.prdDtl.forEach(item => {
-            // 仅在产品名称不在排除列表时，将 prdErn 乘以 5
             if (item.prdErn && !excludeNames.includes(item.prdName)) {
                 item.prdErn = (parseFloat(item.prdErn) * 5).toFixed(2).toString();
             }
         });
+    }
+} else if (url.includes('/iincomereport/income/incomerate')) {
+    if (obj.bizResult?.data?.ernInfo) {
+        let lastEntry = obj.bizResult.data.ernInfo[obj.bizResult.data.ernInfo.length - 1];
+        lastEntry.ernMon = totalSum.toFixed(2).toString(); // 使用总和
     }
 }
 
